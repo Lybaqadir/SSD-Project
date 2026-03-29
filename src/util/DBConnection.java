7c63e1589921bd01146769337bd3a4153ed87ba1
Lyba Qadir <60304987@udst.edu.qa>
2026-03-29 18:04:16 +0300

    Created db connection file

---
+++ src/util/DBConnection.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+
+        if (connection == null || connection.isClosed()) {
+
+            try {
+                Properties props = new Properties();
+                props.load(new FileInputStream("src/config.properties"));
+
+                String url      = props.getProperty("db.url");
+                String user     = props.getProperty("db.user");
+                String password = props.getProperty("db.password");
+
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("Connected to database successfully.");
+
+            } catch (Exception e) {
+                System.out.println("Connection failed: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+}
